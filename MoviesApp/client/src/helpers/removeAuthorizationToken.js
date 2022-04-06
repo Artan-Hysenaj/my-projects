aec57c232c4e74fa68c70956be607492cfe84cb5
Artan-Hysenaj <ah47077@ubt-uni.net>
2022-04-06 18:51:10 +0200

    asd

--- MoviesApp/client/src/helpers/removeAuthorizationToken.js
+++
@@ -1,5 +0,0 @@
-import { axiosInstance } from "../api/api";
-
-export default function removeAuthorizationToken() {
-  delete axiosInstance.defaults.headers.common.authorization;
-}
--- Vue - The Complete Guide (incl. Router & Composition API)/README.md
+++
@@ -1,2 +0,0 @@
-### `VUE application demo`
-[DEMO](https://vue-http-demo-49523.web.app/coaches)
