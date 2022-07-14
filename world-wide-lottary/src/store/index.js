a23736a76d657ca57f636e6f1f4c6bcbca0b02c6
Artan-Hysenaj <83249411+artanhysenaj@users.noreply.github.com>
2022-07-15 01:07:14 +0200

    project init

--- world-wide-lottary/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import uiSlice from "./ui-slice";
-import userSlice from "./user-slice";
-const store = configureStore({
-  reducer: { user: userSlice.reducer, ui: uiSlice.reducer },
-});
-export default store;
